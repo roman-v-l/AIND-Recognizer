2bb9712a7e563f78420622b18ef68c09d566dc63
roman_v_l <rlototskyy@gmail.com>
2017-05-23 18:05:31 -0400

    LM implementation

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -15,8 +15,8 @@ class ModelSelector(object):
     '''
 
     def __init__(self, all_word_sequences: dict, all_word_Xlengths: dict, this_word: str,
-                 n_constant=3,
-                 min_n_components=2, max_n_components=10,
+                 n_constant=2,
+                 min_n_components=2, max_n_components=30,
                  random_state=14, verbose=False):
         self.words = all_word_sequences
         self.hwords = all_word_Xlengths
@@ -77,9 +77,37 @@ class SelectorBIC(ModelSelector):
         """
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection based on BIC scores
-        raise NotImplementedError
+        # The list of BIC scores for the models
+        lBICs = list()
+        for n_components in range(self.min_n_components, self.max_n_components + 1):
+            model = self.base_model(n_components)
+            try:
+                if model:
+                    logL = model.score(self.X, self.lengths)
 
+                    # Calculate the number of parameter, p, in the BIC criteria
+                    p = n_components * n_components + 2 * n_components * len(self.X[0]) - 1
+                    BIC = -2.0 * logL + p * np.log(len(self.X))
+                    lBICs.append([n_components, BIC, model])
+            except:
+                # The score method throws an exception, ignore the current model
+                pass
+
+        if self.verbose:
+            print("BIC values [states, BIC]: {}".format(lBICs))
+
+        # If the list is empty, GaussianHMM could not train any models for the given word
+        if len(lBICs) == 0:
+            return None
+
+        # Get the model with the minimum BIC score
+        n_components, BIC, model = min(lBICs, key=lambda e: e[1])
+
+        if self.verbose:
+            print("Min BIC is {} when {} states".format(BIC, n_components))
+            print("Best model: {}", model)
+
+        return model
 
 class SelectorDIC(ModelSelector):
     ''' select best model based on Discriminative Information Criterion
@@ -93,9 +121,49 @@ class SelectorDIC(ModelSelector):
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection based on DIC scores
-        raise NotImplementedError
+        lDICs = list()
+        for n_components in range(self.min_n_components, self.max_n_components + 1):
+            model = self.base_model(n_components)
+            if model:
+                try:
+                    # Get the average likelihood value for self.this_word
+                    logL = model.score(self.X, self.lengths) / len(self.lengths)
+                except:
+                    # Can't get the score - skip this model
+                    continue
 
+                sum_logL = 0.0
+                count = 0
+                for word in self.hwords.keys():
+                    if word is not self.this_word:
+                        try:
+                            other_X, other_lengths = self.hwords[word]
+                            # Get the average anti-likelihood value for word
+                            sum_logL += model.score(other_X, other_lengths) / len(other_lengths)
+                            count += 1
+                        except:
+                            # Can't get the score - skip this model
+                            pass
+
+                DIC = logL
+                if self.verbose:
+                    print("likelihood: {}".format(logL))
+                if count > 0:
+                    # Get the DIC score
+                    DIC -= sum_logL / count
+                    if self.verbose:
+                        print("anti likelihood: {}".format(sum_logL / count))
+
+                lDICs.append([n_components, DIC, model])
+
+        # If the list is empty, GaussianHMM could not train any models for the given word
+        if len(lDICs) == 0:
+            return None
+
+        # Get the model with the maximum DIC score
+        n_components, DIC, model = max(lDICs, key=lambda e: e[1])
+
+        return model
 
 class SelectorCV(ModelSelector):
     ''' select best model based on average log Likelihood of cross-validation folds
@@ -108,19 +176,22 @@ class SelectorCV(ModelSelector):
         if len(self.lengths) == 1:
             return self.base_model(self.n_constant)
 
-        split_method = KFold(n_splits=min(3,len(self.lengths)))
-        best_logL = -sys.maxsize - 1
+        # Create 3-fold split method (if possible)
+        split_method = KFold(min(3, len(self.lengths)))
+        best_logL = float('-inf')
         best_n_components = 0
         for n_components in range(self.min_n_components, self.max_n_components + 1):
             total_logL = 0
             count = 0
             for cv_train_idx, cv_test_idx in split_method.split(self.sequences):
+                # Use the training Xlength values
                 self.X, self.lengths = combine_sequences(cv_train_idx, self.sequences)
                 model = self.base_model(n_components)
 
                 try:
                     if model:
                         test_X, test_lengths = combine_sequences(cv_test_idx, self.sequences)
+                        # Get the likelihood score
                         total_logL += model.score(test_X, test_lengths)
                         count += 1
                 except:
@@ -130,6 +201,7 @@ class SelectorCV(ModelSelector):
 
 
             if count > 0:
+                # Get the average likelihood score
                 avg_logL = total_logL / count
                 if self.verbose:
                     print("Average Log {} with {} states".format(avg_logL, n_components))
@@ -137,7 +209,10 @@ class SelectorCV(ModelSelector):
                     best_logL = avg_logL
                     best_n_components = n_components
 
+        # Reset the original XLength values
         self.X, self.lengths = self.hwords[self.this_word]
+
+        # Return the winning model trained on the full training set
         return self.base_model(best_n_components)
 
 
@@ -158,10 +233,11 @@ if __name__ == '__main__':
     sequences = training.get_all_sequences()
     Xlengths = training.get_all_Xlengths()
 
-    word = 'CHICKEN'
+    #print(Xlengths)
+    word = 'BOOK'
 
     start = timeit.default_timer()
-    model = SelectorCV(sequences, Xlengths, word,
+    model = SelectorDIC(sequences, Xlengths, word,
                        min_n_components=2, max_n_components=15, random_state=14, verbose=True).select()
     end = timeit.default_timer() - start
 
--- my_recognizer.py
+++ my_recognizer.py
@@ -2,7 +2,7 @@ import warnings
 from asl_data import SinglesData
 import operator
 import sys
-
+import arpa
 
 def recognize(models: dict, test_set: SinglesData):
     """ Recognize test word sequences from word models set
@@ -41,11 +41,115 @@ def recognize(models: dict, test_set: SinglesData):
 
 
     return probabilities, guesses
+
+def recognize_lm(probabilities: list, lm, test_set: SinglesData):
+    """ Recognize test word sequences using obraines probabilities and language models
+
+       :param probabilities: is a list of dictionaries where each key a word and value is Log Liklihood
+           [{SOMEWORD': LogLvalue, 'SOMEOTHERWORD' LogLvalue, ... },
+            {SOMEWORD': LogLvalue, 'SOMEOTHERWORD' LogLvalue, ... },
+            ]
+       :param lm : ARPA n-gram model
+       :param test_set: SinglesData object
+       :return: dictionary of recognized sentences: {video_num: sentence}
+       """
+
+    # Order the words by probabilities
+    sorted_probs = list()
+    for i in range(len(probabilities)):
+        probs = [[probabilities[i][word], word] for word in probabilities[i]]
+        sorted_probs.append(sorted(probs, key=lambda t: t[0], reverse=True))
+
+    # Check only top TOP_NUM words
+    TOP_NUM = min(50, len(probabilities[0]))
+    ALPHA = 20.0
+    recognized_sentences = dict()
+    for video_num in test_set.sentences_index:
+        sentence = list()
+        for i in test_set.sentences_index[video_num]:
+            max_score = float('-inf')
+            recognized_word = ''
+            for p_i in range(TOP_NUM):
+                # Get the score from the trained model
+                score = sorted_probs[i][p_i][0]
+
+                try:
+                    # Don't apply LM to the first word in the sentence
+                    if len(sentence) > 0:
+                        str_sentence = "<s> " + " ".join(sentence) + " " + sorted_probs[i][p_i][1]
+
+                        # Get the log of p(current_word|previous_words)
+                        lm_score = lm.log_p(str_sentence)
+
+                        # Combine the scores using ALPHA model factor
+                        score += ALPHA * lm_score
+                except:
+                    continue
+
+                # Pick the word with the highest combined score
+                if score > max_score:
+                    max_score = score
+                    recognized_word = sorted_probs[i][p_i][1]
+
+            # Add the winning word to the sentence
+            sentence.append(recognized_word)
+
+        recognized_sentences[video_num] = sentence
+
+    return recognized_sentences
+
+def show_errors_lm(recognized_sentences: list, test_set: SinglesData):
+    """ Print WER and sentence differences in tabular form
+
+    :param guesses: list of test item answers, ordered
+    :param test_set: SinglesData object
+    :return:
+        nothing returned, prints error report
+
+    WER = (S+I+D)/N  but we have no insertions or deletions for isolated words so WER = S/N
+    """
+
+    S = 0
+    N = len(test_set.wordlist)
+
+    print('Video  Recognized                                                    Correct')
+    print('=====================================================================================================')
+    for video_num in test_set.sentences_index:
+        correct_sentence = [test_set.wordlist[i] for i in test_set.sentences_index[video_num]]
+        recognized_sentence = recognized_sentences[video_num]
+        for i in range(len(recognized_sentence)):
+            if recognized_sentence[i] != correct_sentence[i]:
+                recognized_sentence[i] = '*' + recognized_sentence[i]
+                S += 1
+        print('{:5}: {:60}  {}'.format(video_num, ' '.join(recognized_sentence), ' '.join(correct_sentence)))
+
+    print("\n**** WER = {}".format(float(S) / float(N)))
+    print("Total correct: {} out of {}".format(N - S, N))
+
 if __name__ == '__main__':
     from asl_data import AslDb
+    import pandas as pd
+    import os
     import timeit
 
-    asl= AslDb()
+    models = arpa.loadf(os.path.join('data', 'ukn.3.lm'))
+    lm = models[0]
+
+    phrase = "<s> JOHN FISH WONT JOHN"
+    # probability p(end|in, the)
+    print(lm.log_p(phrase))
+
+    # sentence score w/ sentence markers
+    print(lm.log_s(phrase))
+
+    phrase = "<s> JOHN FISH WONT EAT"
+    # probability p(end|in, the)
+    print(lm.log_p(phrase))
+
+    # sentence score w/ sentence markers
+    print(lm.log_s(phrase))
+
+"""asl= AslDb()
 
     asl.df['grnd-ry'] = asl.df['right-y'] - asl.df['nose-y']
     asl.df['grnd-rx'] = asl.df['right-x'] - asl.df['nose-x']
@@ -54,14 +158,62 @@ if __name__ == '__main__':
 
     features_ground = ['grnd-rx', 'grnd-ry', 'grnd-lx', 'grnd-ly']
 
-    training = asl.build_training(features_ground)
-    sequences = training.get_all_sequences()
-    Xlengths = training.get_all_Xlengths()
+    asl.df['delta-rx+4'] = asl.df['right-x'].diff(4).fillna(0)
+    asl.df['delta-ry+4'] = asl.df['right-y'].diff(4).fillna(0)
+    asl.df['delta-lx+4'] = asl.df['left-x'].diff(4).fillna(0)
+    asl.df['delta-ly+4'] = asl.df['left-y'].diff(4).fillna(0)
 
-    words_to_train = ['FISH', 'BOOK', 'VEGETABLE', 'FUTURE', 'JOHN']
-    import timeit
+    features_delta_4 = ['delta-rx+4', 'delta-ry+4', 'delta-lx+4', 'delta-ly+4']
+
+    asl.df['delta-rx-4'] = asl.df['right-x'].diff(-4).fillna(0)
+    asl.df['delta-ry-4'] = asl.df['right-y'].diff(-4).fillna(0)
+    asl.df['delta-lx-4'] = asl.df['left-x'].diff(-4).fillna(0)
+    asl.df['delta-ly-4'] = asl.df['left-y'].diff(-4).fillna(0)
+
+    features_delta_4p = ['delta-rx-4', 'delta-ry-4', 'delta-lx-4', 'delta-ly-4']
+
+    features_custom = features_ground + features_delta_4 + features_delta_4p
+
+    df_min = asl.df.groupby('speaker').min()
+    df_max = asl.df.groupby('speaker').max()
+
+
+    def normalize(feature_name, feature_name_norm):
+        feature_name_min = feature_name + "-min"
+        feature_name_max = feature_name + "-max"
+
+        asl.df[feature_name_min] = asl.df['speaker'].map(df_min[feature_name])
+        asl.df[feature_name_max] = asl.df['speaker'].map(df_max[feature_name])
+
+        asl.df[feature_name_norm] = (asl.df[feature_name] - asl.df[feature_name_min]) / (asl.df[feature_name_max] -
+                                                                                         asl.df[feature_name_min])
+
+    normalize('grnd-rx', 'grnd-rx-norm')
+    normalize('grnd-lx', 'grnd-lx-norm')
+    normalize('grnd-ry', 'grnd-ry-norm')
+    normalize('grnd-ly', 'grnd-ly-norm')
+
+    features_ground_norm = ['grnd-rx-norm', 'grnd-ry-norm', 'grnd-lx-norm', 'grnd-ly-norm']
+
+    normalize('delta-rx+4', 'delta-rx+4-norm')
+    normalize('delta-lx+4', 'delta-lx+4-norm')
+    normalize('delta-ry+4', 'delta-ry+4-norm')
+    normalize('delta-ly+4', 'delta-ly+4-norm')
+
+    features_delta_4_norm = ['delta-rx+4-norm', 'delta-ry+4-norm', 'delta-lx+4-norm', 'delta-ly+4-norm']
+
+    normalize('delta-rx-4', 'delta-rx-4-norm')
+    normalize('delta-lx-4', 'delta-lx-4-norm')
+    normalize('delta-ry-4', 'delta-ry-4-norm')
+    normalize('delta-ly-4', 'delta-ly-4-norm')
+
+    features_delta_4p_norm = ['delta-rx-4-norm', 'delta-ry-4-norm', 'delta-lx-4-norm', 'delta-ly-4-norm']
+
+    features_custom_norm = features_ground_norm + features_delta_4_norm + features_delta_4p_norm
 
     from my_model_selectors import SelectorCV
+    from my_model_selectors import SelectorBIC
+    from my_model_selectors import SelectorDIC
     from my_model_selectors import SelectorConstant
 
 
@@ -77,11 +229,17 @@ if __name__ == '__main__':
         return model_dict
 
 
-    models = train_all_words(features_ground, SelectorCV)
+    features = features_custom_norm
+
+    models = train_all_words(features, SelectorBIC)
     print("Number of word models returned = {}".format(len(models)))
 
     from asl_utils import show_errors
 
-    test_set = asl.build_test(features_ground)
+    test_set = asl.build_test(features)
     probabilities, guesses = recognize(models, test_set)
     show_errors(guesses, test_set)
+
+    df_probs = pd.DataFrame(data=probabilities)
+    df_probs.head()
+"""
